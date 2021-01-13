aed02e7d5c21dfca604d877880659d64ccfd0572
Leon Zhao <lzhao92@gmail.com>
2021-01-13 18:45:04 -0500

    all drinks backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
-module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/drinks', require('./drinks'))
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
+  const error = new Error('api route not Found')
   error.status = 404
   next(error)
 })
+
+module.exports = router

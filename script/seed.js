fb2f06a7c3597623aed05fabd3734060fe859e72
DNaomi <72106582+DNaomi@users.noreply.github.com>
2021-01-15 17:16:26 -0500

    updated seed with orders and orderItems

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const {User, Tea} = require('../server/db/models')
+const {User, Tea, Order, OrderItem} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -9,15 +9,51 @@ async function seed() {
 
   const users = await Promise.all([
     User.create({
-      firstName: 'mati',
-      lastName: 'dejene',
-      email: 'cody@email.com',
+      firstName: 'Mati',
+      lastName: 'Dejene',
+      email: 'mati@email.com',
       password: '123'
     }),
     User.create({
-      firstName: 'mati',
-      lastName: 'dejene',
-      email: 'murphy@email.com',
+      firstName: 'M',
+      lastName: 'D',
+      email: 'md@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Eric',
+      lastName: 'Zou',
+      email: 'eric@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'E',
+      lastName: 'Z',
+      email: 'ez@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Leon',
+      lastName: 'Zhao',
+      email: 'Leon@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'L',
+      lastName: 'Z',
+      email: 'lz@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Naomi',
+      lastName: 'Diaz',
+      email: 'naomi@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'N',
+      lastName: 'D',
+      email: 'nd@email.com',
       password: '123'
     })
   ])
@@ -52,9 +88,122 @@ async function seed() {
       flavor: 'coconut',
       topping: 'pineapple',
       size: 'large'
+    }),
+    Tea.create({
+      type: 'black',
+      flavor: 'blueberry',
+      topping: 'jelly',
+      size: 'medium'
+    }),
+    Tea.create({
+      type: 'jasmine',
+      flavor: 'mint',
+      topping: 'boba',
+      size: 'large'
+    }),
+    Tea.create({
+      type: 'chai',
+      flavor: '',
+      topping: 'boba',
+      size: 'small'
+    }),
+    Tea.create({
+      type: 'oolong',
+      flavor: 'vanilla',
+      topping: 'boba',
+      size: ' extra large'
+    }),
+    Tea.create({
+      type: 'earl grey',
+      flavor: 'coconut',
+      topping: 'rasberry',
+      size: 'medium'
     })
   ])
 
+  const orders = await Promise.all([
+    Order.create({
+      userId: 1
+    }),
+    Order.create({
+      userId: 2
+    }),
+    Order.create({
+      userId: 2
+    }),
+    Order.create({
+      userId: 1
+    }),
+    Order.create({
+      userId: 6
+    }),
+    Order.create({
+      userId: 3
+    }),
+    Order.create({
+      userId: 4
+    }),
+    Order.create({
+      userId: 5
+    }),
+    Order.create({
+      userId: 6
+    }),
+    Order.create({
+      userId: 4
+    })
+  ])
+
+  const orderItems = await Promise.all([
+    OrderItem.create({
+      orderId: 1,
+      teaId: 1,
+      quantity: 3
+    }),
+    OrderItem.create({
+      orderId: 1,
+      teaId: 2,
+      quantity: 7
+    }),
+    OrderItem.create({
+      orderId: 3,
+      teaId: 2,
+      quantity: 1
+    }),
+    OrderItem.create({
+      orderId: 4,
+      teaId: 1,
+      quantity: 1
+    }),
+    OrderItem.create({
+      orderId: 5,
+      teaId: 2,
+      quantity: 2
+    }),
+    OrderItem.create({
+      orderId: 6,
+      teaId: 3,
+      quantity: 2
+    }),
+    OrderItem.create({
+      orderId: 7,
+      teaId: 4,
+      quantity: 4
+    }),
+    OrderItem.create({
+      orderId: 8,
+      teaId: 5,
+      quantity: 1
+    }),
+    OrderItem.create({
+      orderId: 9,
+      teaId: 5,
+      quantity: 1
+    })
+  ])
+
+  console.log(`seeded ${orderItems.length} orderItems`)
+  console.log(`seeded ${orders.length} orders`)
   console.log(`seeded ${users.length} users`)
   console.log(`seeded ${teas.length} teas`)
   console.log(`seeded successfully`)

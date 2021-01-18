01459aaa6246ae3b78af7ca8d8dddfc55f815dd3
Eric Zou <ericczzou@gmail.com>
2021-01-18 17:39:28 -0500

    added Order instance on sign up, fixed sign up form

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -34,6 +34,45 @@ const AuthForm = props => {
   )
 }
 
+const AuthForm2 = props => {
+  const {name, displayName, handleSubmit, error} = props
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+      <a href="/auth/google">{displayName} with Google</a>
+    </div>
+  )
+}
 /**
  * CONTAINER
  *   Note that we have two different sets of 'mapStateToProps' functions -
@@ -69,8 +108,22 @@ const mapDispatch = dispatch => {
   }
 }
 
+const mapDispatchSignup = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const formName = evt.target.name
+      const lastName = evt.target.lastName.value
+      const firstName = evt.target.firstName.value
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      dispatch(auth(email, password, formName, firstName, lastName))
+    }
+  }
+}
+
 export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Signup = connect(mapSignup, mapDispatchSignup)(AuthForm2)
 
 /**
  * PROP TYPES
